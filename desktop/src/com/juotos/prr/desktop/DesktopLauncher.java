d9bbdfc2dd1cc29162543ef14a7110106ddec619
Joni Ahola <joni@juotos.fi>
2014-11-15 15:27:32 +0200

    Fix LibGDX project

---
+++ desktop/src/com/juotos/prr/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.juotos.prr.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.juotos.prr.PRRGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PRRGame(), config);
+	}
+}
