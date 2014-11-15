1c8971ed32ddba14cfc9f29e626579f01a095620
Joni Ahola <joni@juotos.fi>
2014-11-15 14:09:44 +0200

    Create project with LibGDX

---
+++ desktop/src/com/juotos/portrait-runner-runner/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.juotos.portrait-runner-runner.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.juotos.portrait-runner-runner.PortraitRunnerRunner;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PortraitRunnerRunner(), config);
+	}
+}
