feb52a48963c1bf5eb1b395252ae464966130474
Joni Ahola <joni@juotos.fi>
2014-11-15 23:24:00 +0200

    Add default window size + title to Desktop Launcher

--- desktop/src/com/juotos/prr/desktop/DesktopLauncher.java
+++ desktop/src/com/juotos/prr/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.juotos.prr.PRRGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "portrait-runner-runner";
+		config.width = 480;
+		config.height = 800;
+		
 		new LwjglApplication(new PRRGame(), config);
 	}
 }
