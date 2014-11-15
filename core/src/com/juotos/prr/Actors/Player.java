3fb58f6ef8e742cd82bc5480d08d101da26d7af8
Joni Ahola <joni@juotos.fi>
2014-11-16 00:37:35 +0200

    Add Player actor

---
+++ core/src/com/juotos/prr/Actors/Player.java
@@ -0,0 +1,58 @@
+package com.juotos.prr.Actors;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.juotos.prr.Constants;
+import com.juotos.prr.PRRGame;
+
+public class Player {
+	private static final String PLAYER_ASSETS_DIR = "player/";
+	private static final int MAX_HIT_METER = 100;
+	private static final int MIN_HIT_METER = 0;
+	private static final float HIT_METER_CAP = MAX_HIT_METER / 3;
+	
+	private PRRGame game;
+	
+	public boolean leftSide = true;
+	public boolean jumping = false;
+	public int hitMeter = MAX_HIT_METER;
+	public Texture playerTexture1, playerTexture2, playerTexture3;
+	public int width = 70, height = 70;
+	public float x = 0f, y = 0f;
+	
+	public Player(PRRGame g) {
+		game = g;
+		playerTexture1 = new Texture(Gdx.files.external(Constants.ASSETS_DIR + PLAYER_ASSETS_DIR + "1.png"));
+		playerTexture2 = new Texture(Gdx.files.external(Constants.ASSETS_DIR + PLAYER_ASSETS_DIR + "2.png"));
+		playerTexture3 = new Texture(Gdx.files.external(Constants.ASSETS_DIR + PLAYER_ASSETS_DIR + "3.png"));
+		
+		x = Constants.GAME_WIDTH / 2;
+		y = Constants.GAME_HEIGHT - height; //bottom to screen
+	}
+	public void update() {
+		
+	}
+	
+	public void increaseHealth(int value) {
+		hitMeter += value;
+		if(hitMeter > MAX_HIT_METER) hitMeter = MAX_HIT_METER;
+	}
+	public void decreaseHealth(int value) {
+		hitMeter -= value;
+		if(hitMeter <= MIN_HIT_METER) {
+			hitMeter = 0;
+			game.toggleGameState();
+		}
+	}
+	
+	public Texture currentPlayerTexture() {
+		if(hitMeter <= HIT_METER_CAP) 
+			return playerTexture3;
+		else if(hitMeter > HIT_METER_CAP && hitMeter <= (2* HIT_METER_CAP))
+			return playerTexture2;
+		else 
+			return playerTexture1;
+	}
+}
--- core/src/com/juotos/prr/PRRGame.java
+++ core/src/com/juotos/prr/PRRGame.java
@@ -11,13 +11,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.ui.List;
+import com.juotos.prr.Actors.Player;
 
 public class PRRGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture woodenTexture1, woodenTexture2, woodenTexture3, woodenTexture4, woodenTexture5, woodenTexture6;
-	Texture playerTexture1, playerTexture2, playerTexture3;
+	Player player;
+	private GameState state = GameState.RUN;
 	
-	int playerSize[] = {70, 70}; //width, height
 	int woodenSize[][] = {
 			{70, 70, 70, 70, 70, 70}, 
 			{140, 220, 140, 220, 140, 220}
@@ -30,6 +31,7 @@ public class PRRGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		camera = new OrthographicCamera();
+		player = new Player(this);
 		initializeAssets();
 		
 		camera.setToOrtho(false, 480, 800);
@@ -37,17 +39,51 @@ public class PRRGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+		switch(state) {
+			case RUN:
+				renderRun();
+				break;
+			case PAUSE:
+				renderPause();
+				break;
+			case RESUME:
+				renderResume();
+				break;
+			case STOPPED:
+				renderStopped();
+				break;
+		}
+			
+	}
+	
+	private void renderRun() {
 		Gdx.gl.glClearColor(0.5f, 0.5f, 1, 1);	//red,green,blue,alpha
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //Clear screen
 		camera.update();
 		anyKeyPressed();
 		
 		batch.setProjectionMatrix(camera.combined);
+		
 		batch.begin();
 		batchUpdate();
 		batch.end();
+		
+		player.update();
+		
 	}
+	private void renderPause() {
+		
+	}
+	private void renderResume() {
+		
+	}
+	private void renderStopped() {
 	
+	}
+	public void toggleGameState() {
+		//Toggle pause or run state
+		state = (state == GameState.RUN) ? GameState.PAUSE : GameState.RUN;
+	}
 	private void initializeAssets() {
 		woodenTexture1 = new Texture(Gdx.files.external("Development/game-dev/portrait-runner-runner/assets/wood/1.png"));
 		woodenTexture2 = new Texture(Gdx.files.external("Development/game-dev/portrait-runner-runner/assets/wood/2.png"));
@@ -55,10 +91,6 @@ public class PRRGame extends ApplicationAdapter {
 		woodenTexture4 = new Texture(Gdx.files.external("Development/game-dev/portrait-runner-runner/assets/wood/4.png"));
 		woodenTexture5 = new Texture(Gdx.files.external("Development/game-dev/portrait-runner-runner/assets/wood/5.png"));
 		woodenTexture6 = new Texture(Gdx.files.external("Development/game-dev/portrait-runner-runner/assets/wood/6.png"));
-		
-		playerTexture1 = new Texture(Gdx.files.external("Development/game-dev/portrait-runner-runner/assets/player/1.png"));
-		playerTexture2 = new Texture(Gdx.files.external("Development/game-dev/portrait-runner-runner/assets/player/2.png"));
-		playerTexture3 = new Texture(Gdx.files.external("Development/game-dev/portrait-runner-runner/assets/player/3.png"));
 	}
 	
 	private void anyKeyPressed() {
@@ -67,8 +99,8 @@ public class PRRGame extends ApplicationAdapter {
 		}
 	}
 	private void batchUpdate() {
-		
-		
+		batch.draw(player.currentPlayerTexture(), player.x, player.y);
 		
 	}
+
 }
